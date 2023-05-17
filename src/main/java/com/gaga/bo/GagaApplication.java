e1ce06e07fa4a46a74844d7924c557229efffc9c
yu-bin-820 <jyb9994@naver.com>
2023-05-17 15:31:28 +0900

    create : 5/17

---
+++ src/main/java/com/gaga/bo/GagaApplication.java
@@ -0,0 +1,13 @@
+package com.gaga.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaga/bo/GagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaga.bo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
